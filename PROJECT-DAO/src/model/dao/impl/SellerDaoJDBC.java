402df0e050c9fbfac2cdc8542265ff7dbacda2c6
jaum <juaun1981@gmail.com>
2024-07-23 10:23:55 -0300

    criacao de metodo para instanciar seler e department em SellerDaoJDBC

--- PROJECT-DAO/src/model/dao/impl/SellerDaoJDBC.java
+++ PROJECT-DAO/src/model/dao/impl/SellerDaoJDBC.java
@@ -53,16 +53,8 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1, id);
             rs = st.executeQuery();
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("Name"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null; // caso nao tenha executado nada(ex: seller vazio)
@@ -74,6 +66,24 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeConnection();
         }
 
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException { // propaga a excessao
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("Name"));
+        return dep;
     }
 
     @Override

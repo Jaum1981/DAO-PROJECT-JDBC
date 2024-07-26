b7367f05726db9f10617bf92ba7b584b0d111373
jaum <juaun1981@gmail.com>
2024-07-26 07:49:33 -0300

    implementacao do delete by id

--- PROJECT-DAO/src/application/Program.java
+++ PROJECT-DAO/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); // conhece somente a interface e nao a implementacao
 
@@ -36,6 +39,14 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("=== TEST 4: seller findAll ===");
+        System.out.print("Enter the id that you want to delete from DataBase: ");
+        Integer idDel = sc.nextInt();
+        sellerDao.deleteById(idDel);
+        System.out.println("Deleted");
+
+        sc.close();
 
     }
 }
